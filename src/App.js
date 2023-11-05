31036c5186d270c1fe6c566f9c157caa2c3b7ba4
Baetti <georgebasil1711@gmail.com>
2023-11-06 00:44:49 +0530

    quiz

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Quiz from "./Components/Quiz";
 function App() {
   return (
     <div
-      style={{ height: "100vh" }}
+      style={{
+        height: "100vh",
+        backgroundImage:
+          "url('https://t3.ftcdn.net/jpg/03/23/12/42/360_F_323124237_SfGqpttZqU2mrMm61VPSWA2tKvc95l9O.jpg')",
+
+        backgroundSize: "cover",
+      }}
       className="d-flex align-items-center justify-content-center w-100"
     >
-      <div className="col-lg-6 shadow rounded bg-slate-300 ">
+      <div
+        className="col-lg-6 shadow rounded bg-slate-300 m-1 "
+        style={{
+          backgroundImage:
+            'url("https://t3.ftcdn.net/jpg/03/08/21/72/360_F_308217222_SkOkOPhWtaAMNFw77Z0JVy4mPSgg4imN.jpg")',
+          backgroundSize: "cover",
+        }}
+      >
         {" "}
         <Quiz />
       </div>
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import { questions } from "./questions";
+import "./Quiz.css";
 
 function Quiz() {
-  const [activeQuestion, setActiveQuestions] = useState(0);
+  const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [showResult, setShowResult] = useState(false);
   const [result, setResult] = useState({
-    score: "",
-    correctAnswers: "",
-    wrongAnswers: "",
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
   });
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
 
-  // const quiz = { questions };
-  const { question, options } = questions[activeQuestion];
+  const { question, options, correctAnswer } = questions[activeQuestion];
 
   const onClickNext = () => {
-    setActiveQuestions((previous) => previous + 1);
     setResult((previous) =>
       selectedAnswer
         ? {
@@ -24,20 +25,101 @@ function Quiz() {
           }
         : { ...previous, wrongAnswers: previous.wrongAnswers + 1 }
     );
+
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((previous) => previous + 1);
+      setSelectedAnswer("");
+      setSelectedAnswerIndex(null);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  const onSelectedAnswer = (answer, index) => {
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+    } else {
+      setSelectedAnswer(false);
+    }
+  };
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
+
+  const resetQuiz = () => {
+    setActiveQuestion(0);
+    setSelectedAnswer("");
+    setShowResult(false);
+    setResult({ score: 0, correctAnswers: 0, wrongAnswers: 0 });
+    setSelectedAnswerIndex(null);
   };
 
   return (
-    <div className="d-flex w-100 justify-content-center flex-column">
-      <h2>quiz</h2>
-      <h2>{question}</h2>
-      <ul>
-        {options.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      <button onClick={onClickNext} className="btn">
-        Next
-      </button>
+    <div
+      style={{ height: "75vh" }}
+      className="d-flex w-100 p-1 justify-content-center flex-column "
+    >
+      {!showResult ? (
+        <>
+          <div>
+            <span className="fs-3 text-warning">
+              {addLeadingZero(activeQuestion + 1)}
+            </span>
+            <span className="fs-4 text-primary">
+              /{addLeadingZero(questions.length)}
+            </span>
+          </div>
+          <div className="px-3 py-1">
+            <h1
+              className="text-center text-primary fw-bolder"
+              style={{ fontFamily: "Caprasimo" }}
+            >
+              Quiz
+            </h1>
+            <h2
+              style={{ fontFamily: "Bacasime Antique" }}
+              className="fw-bolder text-dark"
+            >
+              {question}
+            </h2>
+          </div>
+
+          {options.map((answer, index) => (
+            <div className="px-2" key={answer}>
+              <button
+                style={{ fontFamily: "Bacasime Antique" }}
+                className={`btn w-100 my-1 ${
+                  selectedAnswerIndex === index
+                    ? "bg-success border border-dark"
+                    : "bg-dark border border-white"
+                }`}
+                onClick={() => onSelectedAnswer(answer, index)}
+              >
+                {answer}
+              </button>
+            </div>
+          ))}
+
+          <div className="d-flex justify-content-end px-2 mb-2 quiz-container">
+            <button
+              onClick={onClickNext}
+              className="btn btn-success"
+              disabled={selectedAnswerIndex === null}
+            >
+              {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="d-flex w-100 p-1 justify-content-center flex-column ">
+          <h2>Total Questions: {questions.length}</h2>
+          <h3>Total Score: {result.score}</h3>
+          <h3>Correct: {result.correctAnswers}</h3>
+          <button onClick={resetQuiz} className="btn btn-primary">
+            New Game
+          </button>
+        </div>
+      )}
     </div>
   );
 }
